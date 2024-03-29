76c34f8061af9639e18e3f41a3caa0c16cc1d21c
eclarke <emmetclarke@gmail.com>
2024-03-29 12:42:59 +0100

    Cleaning up settings activity implementation

--- app/src/main/java/com/example/workoutcreatorapp/SettingsActivity.java
+++ app/src/main/java/com/example/workoutcreatorapp/SettingsActivity.java
@@ -1,14 +1,14 @@
 package com.example.workoutcreatorapp;
 
-import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.workout.Equipment;
 import com.workout.WorkoutArguments;
+
+import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -17,40 +17,48 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
-        Resources resources = getResources();
-        Intent intent = getIntent();
-        String[] equipment = intent.getStringArrayExtra("equipment");
+        Set<Equipment> equipmentNeeded = WorkoutArguments.getInstance().getEquipmentNeeded();
 
-//        CheckBox resistanceBandCheckBox = findViewById(R.id.withResistanceBand);
-//        resistanceBandCheckBox.setChecked(MainActivity.NO_SPACE.isResistanceBand());
-//        resistanceBandCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateResistanceBandSetting(b));
-//
-//        CheckBox kettleBellCheckBox = findViewById(R.id.withKettle);
-//        kettleBellCheckBox.setChecked(MainActivity.NO_SPACE.isKettleBell());
-//        kettleBellCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateKettleBellSetting(b));
-//
-//        CheckBox spaceRestrictionsCheckBox = findViewById(R.id.spaceRestrictions);
-//        spaceRestrictionsCheckBox.setChecked(MainActivity.NO_SPACE.isSpaceRestrictions());
-//        spaceRestrictionsCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateSpaceRestrictionSetting(b));
-//
-//        CheckBox chairCheckBox = findViewById(R.id.withChair);
-//        chairCheckBox.setChecked(MainActivity.NO_SPACE.isSpaceRestrictions());
-//        chairCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateChairSetting(b));
+        CheckBox resistanceBandCheckBox = findViewById(R.id.withResistanceBand);
+        resistanceBandCheckBox.setChecked(equipmentNeeded.contains(Equipment.RESISTANCE_BAND));
+        resistanceBandCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateResistanceBandSetting(b));
+
+        CheckBox kettleBellCheckBox = findViewById(R.id.withKettle);
+        kettleBellCheckBox.setChecked(equipmentNeeded.contains(Equipment.KETTLE_BELL));
+        kettleBellCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateKettleBellSetting(b));
+
+        CheckBox spaceRestrictionsCheckBox = findViewById(R.id.spaceRestrictions);
+        spaceRestrictionsCheckBox.setChecked(equipmentNeeded.contains(Equipment.SPACE));
+        spaceRestrictionsCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateSpaceRestrictionSetting(b));
+
+        CheckBox chairCheckBox = findViewById(R.id.withChair);
+        chairCheckBox.setChecked(equipmentNeeded.contains(Equipment.CHAIR));
+        chairCheckBox.setOnCheckedChangeListener((compoundButton, b) -> updateChairSetting(b));
     }
 
-//    private void updateResistanceBandSetting(boolean value){
-//        MainActivity.NO_SPACE.setResistanceBand(value);
-//    }
-//
-//    private void updateKettleBellSetting(boolean value){
-//        MainActivity.NO_SPACE.setKettleBell(value);
-//    }
-//
-//    private void updateSpaceRestrictionSetting(boolean value){
-//        MainActivity.NO_SPACE.setSpaceRestrictions(value);
-//    }
-//
-//    private void updateChairSetting(boolean value){
-//        MainActivity.NO_SPACE.setSpaceRestrictions(value);
-//    }
+    private void updateResistanceBandSetting(boolean value){
+        updateEquipmentNeeded(value, Equipment.RESISTANCE_BAND);
+    }
+
+    private void updateKettleBellSetting(boolean value){
+        updateEquipmentNeeded(value, Equipment.KETTLE_BELL);
+    }
+
+    private void updateSpaceRestrictionSetting(boolean value){
+        updateEquipmentNeeded(value, Equipment.SPACE);
+    }
+
+    private void updateChairSetting(boolean value){
+        updateEquipmentNeeded(value, Equipment.CHAIR);
+    }
+
+    private void updateEquipmentNeeded(boolean value, Equipment equipment) {
+        Set<Equipment> equipmentNeeded = WorkoutArguments.getInstance().getEquipmentNeeded();
+        if(value){
+            equipmentNeeded.add(equipment);
+        }else{
+            equipmentNeeded.remove(equipment);
+        }
+    }
+
 }
